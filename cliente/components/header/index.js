9a6284f826919826a97e1c46b960d0de7f5d2e48
PabloMur <polillomurillo@gmail.com>
2022-09-02 13:12:38 -0300

    sdsd

--- cliente/components/header/index.js
+++ cliente/components/header/index.js
@@ -3,18 +3,30 @@ function header(el) {
   component.innerHTML = `
   <div class="container">
     <div class="logo">
-      <img src="https://argentinaprograma.inti.gob.ar/pluginfile.php/1/theme_moove/logo/1661403264/APLogo-20-20.png"/>
+      <img src="./img/APLogo-20-20.png"/>
     </div>
     <div class="botonera">
       <ul class="redes">
         <li class="red">
-          <a></a>
+          <a href="https://github.com/PabloMur" target="_blank">
+            <img class="logo-red" src="./img/github.png"/>
+          </a>
         </li>
+
         <li class="red">
-          <a></a>
+          <a href="https://www.linkedin.com/in/pablo-nicol%C3%A1s-murillo-57034b189/" target="_blank">
+            <img class="logo-red" src="./img/linkedin.png"/>
+          </a>
         </li>
+
         <li class="red">
-          <a></a>
+          <a href="https://github.com/PabloMur" target="_blank">
+            <img class="logo-red" src="./img/instagram.png"/>
+          </a>
+        </li>
+
+        <li class="red">
+          <button class="login-button">Login</button>
         </li>
       </ul>
     </div>
